7e54e22871d5688454899d932fee99fc0c9077c7
Mike Richards <mrichards@apterainc.com>
2017-11-28 16:25:00 -0500

    Added FacialExpression model
    Updated ViewController to manage the face based on the FacialExpression state.

---
+++ faceit-swift/FacialExpression.swift
@@ -0,0 +1,45 @@
+//
+//  FacialExpression.swift
+//  faceit-swift
+//
+//  Created by Michael Richards on 11/28/17.
+//  Copyright © 2017 Michael Richards. All rights reserved.
+//
+
+import Foundation
+
+struct FacialExpression {
+    
+    enum Eyes: Int {
+        case open
+        case closed
+        case squinting
+    }
+    
+    enum Mouth : Int {
+        case frown
+        case smirk
+        case neutral
+        case grin
+        case smile
+        
+        var sadder: Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .frown
+        }
+        
+        var happier: Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .smile
+        }
+    }
+    
+    var sadder: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.sadder)
+    }
+    
+    var happier: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.happier)
+    }
+    
+    let eyes: Eyes
+    let mouth: Mouth
+}
